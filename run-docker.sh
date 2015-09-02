3f3a3b78009cc1bc26c71beeacadb63276039910
Sean_Chuang <sean_chuang@htc.com>
2015-09-02 11:47:03 +0800

    1. For easy update

--- run-docker.sh
+++ run-docker.sh
@@ -2,9 +2,13 @@
 DOCKER_IMG="demo/32bit"
 DEMO_CONTAINER_NAME="demo-service"
 UPLOAD_FOLDER=$(pwd)/uploads
+OMRON=$(pwd)/omron
+FLASK=$(pwd)/flask-server
 
 docker run -d \
    --name $DEMO_CONTAINER_NAME \
-   -v $UPLOAD_FOLDER:/opt/flask-server/uploads \
+   -v $OMRON:/opt/omron \
+   -v $FLASK:/opt/flask-server \
+   -v $UPLOAD_FOLDER:/opt/uploads \
    -p 8888:8888 \
    $DOCKER_IMG
